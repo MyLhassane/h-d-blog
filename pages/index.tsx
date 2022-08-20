dad1a6c5f44c68398ff914dba4d21789044f943f
My Lhassane <mastinoz_7@hotmail.com>
2022-08-20 19:12:45 +0100

    Added Some Demo Posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,26 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 
+const posts = [
+  { title: 'React Testing', excerpt: 'Learn React Testing '},
+  { title: 'React with Tailwind', excerpt: 'Learn React with tailwind '},
+]
+
 const Home: NextPage = () => {
   return (
-    <div className="containter mx-auto px-10 mb-8">
+    <div className="container mx-auto px-10 mb-8 bg-gray-300">
       <Head>
-        <title>Create Next App</title>
+        <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        {posts.map((post, index) => (
+          <div>
+            {post.title}
+            {post.excerpt}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
