2c9112dc470332cd5805471249e76f39c86c174e
My Lhassane <mastinoz_7@hotmail.com>
2022-08-20 19:30:31 +0100

    Initialized the main structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,18 +9,28 @@ const posts = [
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        {posts.map((post, index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>
-        ))}
+        {/* Articles */}
+        <div>
+          {posts.map((post, index) => (
+            <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+          ))}
+        </div>
+        {/* Widgets */}
+        <div className='lg:col-span-4 col-span-1'>
+          {/* Recent/Relevent Posts */}
+          <div className='lg:sticky relative top-8'></div>
+          {/* Categiries */}
+          <div className=''></div>
+        </div>
       </div>
     </div>
   )
